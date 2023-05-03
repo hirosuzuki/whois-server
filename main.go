d8b0235cf6a40041e236b0f38fde80de08e94f37
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-05-03 15:22:15 +0000

    Web API server setting

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/likexian/whois"
 	whoisparser "github.com/likexian/whois-parser"
 )
 
-func main() {
+type WhoisInfo struct {
+	Result struct {
+		Domain      string            `json:"domain"`
+		Nameservers []string          `json:"nameservers"`
+		Attributes  map[string]string `json:"attributes"`
+		Raw         string            `json:"raw"`
+	} `json:"result"`
+	Cached  bool   `json:"cached"`
+	Fetched string `json:"fetched"`
+}
+
+func getWhoisInfo(domain string) (*WhoisInfo, error) {
 	result, err := whois.Whois("google.com")
 	if err == nil {
 		fmt.Println(result)
@@ -16,4 +31,31 @@ func main() {
 			fmt.Println(result.Domain.Punycode)
 		}
 	}
+	info := WhoisInfo{}
+	return &info, nil
+}
+
+func getDomainWhois(c echo.Context) error {
+	domain := c.Param("domain")
+	whoisInfo, err := getWhoisInfo(domain)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Error fetching Whois information"})
+	}
+	return c.JSON(http.StatusOK, whoisInfo)
+}
+
+func main() {
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/whois/:domain", getDomainWhois)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
